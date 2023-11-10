e06a29f21beb68cb8c0d9116f440dbe5a02987f2
saidr <saidradwanmansour@hotmail.com>
2023-11-10 19:54:56 +0100

    Github test

---
+++ BirthdayCollector/Controllers/BirthdayCollectorController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BirthdayCollector.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class BirthdayCollectorController : ControllerBase
+{
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public string Get()
+    {
+        Console.WriteLine("Test Github");
+        return "Hello World";
+    }
+}
+
