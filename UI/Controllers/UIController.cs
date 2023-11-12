c615087d3bd37f524d16f83227bc5ec855837c6c
saidr <saidradwanmansour@hotmail.com>
2023-11-12 13:13:34 +0100

    Testing git

--- UI/Controllers/UIController.cs
+++ UI/Controllers/UIController.cs
@@ -60,7 +60,7 @@ public class UIController : Controller
             }
             else
             {
-                MonitorService.Log.Here().Error("Hello");
+                MonitorService.Log.Here().Error("Error");
             }
 
             return View("Index");
